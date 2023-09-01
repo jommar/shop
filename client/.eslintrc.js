b41d2960bc82cdc82c131670d6d7d7d3c9f753d2
Jommar Ilagan <jom102386@gmail.com>
2023-09-01 13:32:47 +0800

    Add rules

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -12,6 +12,13 @@ module.exports = {
     indent: ['error', 2], // 2-space indentation
     semi: ['error', 'never'], // enforces no semicolons
     quotes: ['error', 'single'], // enforces single quotes
+    eqeqeq: ['error', 'always'], // Requires the use of === and !==.
+    'no-else-return': 'error', // Disallows else blocks following if blocks ending with a return statement.
+    'no-var': 'error', // Requires let or const instead of var.
+    'prefer-const': 'error', // If a variable is never reassigned, const is preferred.
+    'vue/no-unused-components': 'error', // Disallows unused components.
+    'vue/require-prop-types': 'error', // Requires prop types to be defined.
+    'max-lines': ['error', 300], // Limits the number of lines in a file.
     // Add more custom rules here
   },
   globals: {
